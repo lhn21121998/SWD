48ffebdccdd92c99193abecfdfaa923a0a0fc2be
linh <lhn21121998@gmail.com>
2020-03-21 09:06:55 +0700

    update code

---
+++ shoesinventorymanagement/src/main/java/com/swd/shoesinventorymanagement/ShoesinventorymanagementApplication.java
@@ -0,0 +1,13 @@
+package com.swd.shoesinventorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesinventorymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesinventorymanagementApplication.class, args);
+	}
+
+}
