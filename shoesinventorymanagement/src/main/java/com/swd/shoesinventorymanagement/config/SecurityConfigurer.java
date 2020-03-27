10f11dd354d5bd5c1569ca8fdbbd0effb890a6b0
huuhiep2505 <48766272+huuhiep2505@users.noreply.github.com>
2020-03-27 21:36:30 +0700

    update code for jwt

--- shoesinventorymanagement/src/main/java/com/swd/shoesinventorymanagement/config/SecurityConfigurer.java
+++ shoesinventorymanagement/src/main/java/com/swd/shoesinventorymanagement/config/SecurityConfigurer.java
@@ -1,5 +1,6 @@
 package com.swd.shoesinventorymanagement.config;
 
+import com.swd.shoesinventorymanagement.filters.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,16 +8,20 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.swd.shoesinventorymanagement.services.MyUserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
 
 	@Autowired
 	private MyUserDetailsService myUserDetailsService;
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -27,7 +32,10 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
 		.antMatchers("/authenticate").permitAll()
-		.anyRequest().authenticated();
+		.anyRequest().authenticated().and().sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 
 	@Override
